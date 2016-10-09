0e8d5e3712737c5e0b4d8a55bf1fd133374a7f2b
audiofreak9 <audiofreak9@gmail.com>
2016-10-08 20:36:38 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+if (($v) && ($v != "l")) $v = "";
+$timeColor = ($v) ?  "EEE" : "222";
+$bgcolor = ($v) ?  "FFF" : "000";
+$color = ($v) ?  "B4B4B4" : "ABABAB";
+$IOcolor = ($v) ?  "333333" : "ABABAB";
+if (!$Lat) $Lat = "Default_Lat";
+if (!$Lng) $Lng = "Default_Lon";
+if (!$WUarea) $WUarea = "Your Town, State";
+?>
